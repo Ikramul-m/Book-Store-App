af81842e0dc827f5ec2abc57e8f99075a1fed63a
Ikramul-m <i20727172@gmail.com>
2023-11-04 12:55:08 +0530

    sixth commit

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
-import express, { request } from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookmodel.js";
 
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
 
 app.get("/", (request, response) => {
   console.log(request);
@@ -36,6 +40,23 @@ app.post('/books', async (request,response) => {
     console.log(error.message);
     response.status(500).send({ message: error.message }); 
   }
+});
+
+
+// Route for Get All Books from database
+
+app.get('/books', async (request,response) => {
+  try {
+    const books = await Book.find({});
+    
+    return response.status(200).json({
+      count: books.length,
+      data: books
+    });
+  } catch(error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message }); 
+  }
 })
 
 mongoose
@@ -50,3 +71,4 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
